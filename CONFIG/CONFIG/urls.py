36ac8378cfd0291023bf8e75c6e1aa9ef635da8a
Berkay Şen <brktrl@protonmail.ch>
2022-02-18 12:05:26 +0300

    added login and register

--- CONFIG/CONFIG/urls.py
+++ CONFIG/CONFIG/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include("PAGESAPP.urls"))
+    path("",include("PAGESAPP.urls")),
+    path("user/",include("UserAPP.urls"))
 ]
 urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- CONFIG/UserAPP/views.py
+++ CONFIG/UserAPP/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+class LoginView(View):
+    http_method_names = ["get","post"]
+    def get(self,request):
+        return render(request,"login.html")
+    def post(self,reqeust):
+        pass
+
+class RegisterView(View):
+    http_method_names = ["get","post"]
+    def get(self,request):
+        return render(request,"register.html")
+    def post(self,request):
+        pass
